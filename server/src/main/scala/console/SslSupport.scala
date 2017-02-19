454ca68879e2f626a63ae89858f0da726eafb288
Vadim Bondarev <haghard84@gmail.com>
2017-02-19 12:08:28 +0100

    add missing files

---
+++ server/src/main/scala/console/SslSupport.scala
@@ -0,0 +1,39 @@
+package console
+
+import java.security._
+import java.io.{InputStream, FileInputStream, File}
+import javax.net.ssl.{KeyManagerFactory, SSLContext, TrustManagerFactory}
+import akka.http.scaladsl.{ConnectionContext, HttpsConnectionContext}
+
+trait SslSupport {
+  val algorithm = "SunX509"
+
+  def sslFile: String
+
+  private def create(in: InputStream, keyPass: String, storePass: String) = {
+    val keyStore = KeyStore.getInstance("JKS")
+    keyStore.load(in, storePass.toCharArray)
+
+    val keyManagerFactory = KeyManagerFactory.getInstance(algorithm)
+    keyManagerFactory.init(keyStore, keyPass.toCharArray)
+
+    val tmf = TrustManagerFactory.getInstance(algorithm)
+    (tmf init keyStore)
+
+    val sslContext = SSLContext.getInstance("TLS")
+    sslContext.init(keyManagerFactory.getKeyManagers, tmf.getTrustManagers, new SecureRandom)
+
+    (ConnectionContext https sslContext)
+  }
+
+  def https(keyPass: String, storePass: String): HttpsConnectionContext = {
+    val file = new File("./"  + sslFile)  //  "./linguistic.jks"
+    if(file.exists) create(new FileInputStream(file), keyPass, storePass)
+    else {
+      resource.managed(getClass.getResourceAsStream("/"+ sslFile))
+        .map { in => create(in, keyPass, storePass) }
+        .opt
+        .fold(throw new Exception("jks file hasn't been found"))(identity)
+    }
+  }
+}
